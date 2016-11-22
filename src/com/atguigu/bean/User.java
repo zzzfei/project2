55a1826309fa473e643cb7eda6aab09d9805b1f6
shyboy_fei <shyboy_fei@sina.com>
2016-11-22 08:42:41 +0800

    第一个测试项目提交

---
+++ src/com/atguigu/bean/User.java
@@ -0,0 +1,16 @@
+package com.atguigu.bean;
+
+/**
+ * Project Name:project1 
+ * File Name:User.java 
+ * Package Name:com.atguigu.bean 
+ * @author shyBoy@fei
+ * Date:2016年11月22日上午8:34:36 
+ */
+public class User {
+	
+	public static void main(String[] args) {
+		System.out.println("helloWorld");
+
+	}
+}
